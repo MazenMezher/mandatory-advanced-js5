2d28ebdf79016b2ac5a721cd00c6427e95acd0ea
Mohammed Motar <mohammed.motar.mm@gmail.com>
2020-03-12 18:49:33 +0100

    Updates

---
+++ precache-manifest.b24e39708fc8ea8f75f4bdb7772f843d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "28fecf943dcee82bbf1076160a9f2fb8",
+    "url": "/mandatory-advanced-js5/index.html"
+  },
+  {
+    "revision": "a0443606bb2f4b30fb7f",
+    "url": "/mandatory-advanced-js5/static/css/main.6fe13488.chunk.css"
+  },
+  {
+    "revision": "1a5680e0c33bdf0f69a3",
+    "url": "/mandatory-advanced-js5/static/js/2.f600bfe5.chunk.js"
+  },
+  {
+    "revision": "0bdc4f0d8b3d84ae6a73bb9902b479ee",
+    "url": "/mandatory-advanced-js5/static/js/2.f600bfe5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a0443606bb2f4b30fb7f",
+    "url": "/mandatory-advanced-js5/static/js/main.bd31d22f.chunk.js"
+  },
+  {
+    "revision": "d348669e83b4c976a4b0",
+    "url": "/mandatory-advanced-js5/static/js/runtime-main.e6b84de9.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/deployment/precache-manifest.02cbece1e0e5121af328adcd925259cb.js"
+  "/mandatory-advanced-js5/precache-manifest.b24e39708fc8ea8f75f4bdb7772f843d.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/deployment/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/mandatory-advanced-js5/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
