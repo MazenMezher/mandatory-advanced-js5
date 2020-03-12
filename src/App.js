b3ec9ddf76ea9db7438fc8e044071a811492fb57
Mohammed Motar <mohammed.motar.mm@gmail.com>
2020-03-12 19:50:28 +0100

    h

--- src/App.js
+++ src/App.js
@@ -10,11 +10,11 @@ import GetToken from "./Components/GetToken";
 function App() {
   return (
     <div className="App">
-      <Router>
-        <Route exact path="/mandatory-advanced-js5" component={LogIn} />
-        <Route path="/mandatory-advanced-js5/auth" component={GetToken} />
-        <Route exact path="/mandatory-advanced-js5/main" component={Main} />
-        <Route exact path="/mandatory-advanced-js5/main/:path+" component={Main} />
+      <Router basename={process.env.PUBLIC_URL}>
+        <Route exact path="/" component={LogIn} />
+        <Route path="/auth" component={GetToken} />
+        <Route exact path="/main" component={Main} />
+        <Route exact path="/main/:path+" component={Main} />
       </Router>
     </div>
   );
