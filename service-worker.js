9f052f38376a30b92ba739986a66361b53fe0971
Mohammed Motar <mohammed.motar.mm@gmail.com>
2020-03-12 20:15:59 +0100

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/mandatory-advanced-js5/precache-manifest.2e8d9ef97300444d29faecbe3e16b4bc.js"
+  "/mandatory-advanced-js5/precache-manifest.b2716703ffa950126ae1653e9118744f.js"
 );
 
 self.addEventListener('message', (event) => {
